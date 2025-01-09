523dd2bcf43aa74636d34e502fe0a638917de245
Korisnik <Korisnik@DESKTOP-GTMO6PB>
2025-01-09 22:13:31 +0100

    Add project files.

---
+++ Generic Delegates/Program.cs
@@ -0,0 +1,40 @@
+﻿using Generic_Methods;
+using System.Collections.Generic;
+
+
+namespace Generic_Delegates
+{
+
+    public delegate U Action<T, U>(T t, U u);
+
+    public delegate bool Predicate<T>(T t);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> accounts = new List<Account>() {
+                 new Account("Eva", 100m),
+                 new Account("Ida", 200),
+                 new SavingsAccount("Teo", 300m, 5m),
+                 new SavingsAccount("Edo", 400m, 5m)
+                 };
+            decimal total = Accumulate<Account, decimal>(
+            accounts,
+            (a, s) => a.Balance + s,
+            a => a.Balance > 200
+            );
+            Console.WriteLine(total);
+        }
+
+        public static U Accumulate<T, U>(
+ IEnumerable<T> collection,
+ Action<T, U> action, Predicate<T> match)
+        {
+            U total = default(U);
+            foreach (T a in collection)
+                if (match(a))
+                    total = action(a, total); // +=
+            return total;
+        }
+    }
+}
---
+++ Generic Methods/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Generic_Methods
+{
+
+    public interface IAccount
+    {
+        string Name { get; set; }
+        decimal Balance { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account a = new Account("Eva", 100m);
+            Account b = new Account("Ida", 200);
+            SavingsAccount c =
+           new SavingsAccount("Teo", 300m, 5m);
+            SavingsAccount d =
+           new SavingsAccount("Edo", 400m, 5m);
+            Account[] array = { a, b, c };
+            List<Account> accounts =
+           new List<Account>() { a, b, c, d };
+            List<SavingsAccount> savings =
+           new List<SavingsAccount>() { c, d };
+            // savings = accounts;
+            // accounts = savings;
+            // Display(array);
+            Display(accounts);
+            Console.WriteLine("----------");
+            Display(savings);
+
+            Console.WriteLine("Accumulated Values:");
+            decimal total = Accumulate(array);
+            Console.WriteLine(total);
+            total = Accumulate(accounts);
+            Console.WriteLine(total);
+            total = Accumulate(savings);
+            Console.WriteLine(total);
+        }
+
+        static void Display<T>(List<T> list) where T : IAccount
+        {
+            // list.Add(new Account("Gea", 500m));
+            foreach (var account in list)
+                Console.WriteLine(account);
+        }
+
+        static decimal Accumulate<T>(IEnumerable<T> collection)
+    where T : Account
+        {
+            decimal total = 0;
+            foreach (var account in collection)
+                total += account.Balance;
+
+            return total;
+        }
+    }
+}
